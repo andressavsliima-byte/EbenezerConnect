2150235fbf2a270b1de86fc51c440cf92462f5ef
andressavsliima-byte <andressavs.liima@gmail.com>
2026-02-06 11:20:48 -0300

    Update upload.js

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -5,17 +5,12 @@ import cloudinary from '../config/cloudinary.js';
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'produtos', // nome da pasta no Cloudinary
+    folder: 'ebenezer/uploads',
     allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
-    transformation: [{ quality: 'auto', fetch_format: 'auto' }],
   },
 });
 
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
-  },
-});
+const upload = multer({ storage });
 
 export default upload;
+
